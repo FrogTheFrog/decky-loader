6cd89ea098dfeb7e0193161f37dfdeb6d6fba334
FrogTheFrog <warliukz@gmail.com>
2023-12-10 20:48:18 +0200

    Welp

--- frontend/src/plugin-loader.tsx
+++ frontend/src/plugin-loader.tsx
@@ -426,29 +426,30 @@ class PluginLoader extends Logger {
       openFilePicker: this.openFilePicker,
       openFilePickerV2: this.openFilePickerV2,
       async callPluginMethod(methodName: string, args = {}, timeout_ms: number = 60 * 1000) {
-        try {
-          console.log('BEFORE FETCH', `http://127.0.0.1:1337/plugins/${pluginName}/methods/${methodName}`);
-          const response = await fetch(`http://127.0.0.1:1337/plugins/${pluginName}/methods/${methodName}`, {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-              'Content-Type': 'application/json',
-              Authentication: window.deckyAuthToken,
-            },
-            body: JSON.stringify({
-              args,
-            }),
-            signal: AbortSignal.timeout(timeout_ms),
-          });
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), timeout_ms);
 
-          console.log('AFTER FETCH', methodName);
-          console.log('BEFORE response.json()');
-          const wud = response.json();
-          console.log('AFTER response.json()');
-          return wud;
-        } catch (error) {
-          console.error(error);
-        }
+        console.log('BEFORE FETCH', `http://127.0.0.1:1337/plugins/${pluginName}/methods/${methodName}`);
+        const response = await fetch(`http://127.0.0.1:1337/plugins/${pluginName}/methods/${methodName}`, {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+            Authentication: window.deckyAuthToken,
+          },
+          body: JSON.stringify({
+            args,
+          }),
+          signal: controller.signal,
+        });
+
+        clearTimeout(timeoutId);
+
+        console.log('AFTER FETCH', methodName);
+        console.log('BEFORE response.json()');
+        const wud = response.json();
+        console.log('AFTER response.json()');
+        return wud;
       },
       fetchNoCors(url: string, request: any = {}) {
         let args = { method: 'POST', headers: {} };
